09226ad43fef768e9a0d71da6e30d75d7d1451fb
JackfriskDTU <s194527@student.dtu.dk>
2024-11-11 14:30:00 +0100

    feat: add loading of mnist dataset

--- ddpm/preprocess.py
+++ ddpm/preprocess.py
@@ -1,1 +1,30 @@
-#!/usr/bin/env python3
+import mnist
+import numpy as np
+import os
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from torchvision import transforms
+from PIL import Image
+
+def load_mnist(batch_size):
+    train_images = mnist.train_images()
+    train_labels = mnist.train_labels()
+    test_images = mnist.test_images()
+    test_labels = mnist.test_labels()
+
+    train_images = train_images.reshape(-1, 1, 28, 28)
+    test_images = test_images.reshape(-1, 1, 28, 28)
+
+    train_images = torch.tensor(train_images, dtype=torch.float32)
+    train_labels = torch.tensor(train_labels, dtype=torch.int64)
+    test_images = torch.tensor(test_images, dtype=torch.float32)
+    test_labels = torch.tensor(test_labels, dtype=torch.int64)
+
+    train_dataset = TensorDataset(train_images, train_labels)
+    test_dataset = TensorDataset(test_images, test_labels)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
