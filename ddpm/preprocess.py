bd9dc0e295a410ff3f9c2c64795d3782f8e8e56e
Nick <nickjoergensen1@gmail.com>
2024-11-15 14:30:04 +0100

    Finished preprocessing

--- ddpm/preprocess.py
+++ ddpm/preprocess.py
@@ -4,9 +4,9 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset, Subset
 from torchvision import datasets,transforms
 from PIL import Image
+import matplotlib.pyplot as plt
 
 class Preprocess:
-
     def load_mnist(batch_size):
 
         # Load MNIST dataset
@@ -22,30 +22,102 @@ class Preprocess:
         
         return train_loader, test_loader
 
+    class TransformedDataset(torch.utils.data.Dataset):
+        def __init__(self, dataset, transform):
+            self.dataset = dataset
+            self.transform = transform
+
+        def __getitem__(self, idx):
+            image, label = self.dataset[idx]
+            return self.transform(image), label
+
+        def __len__(self):
+            return len(self.dataset)
 
     def preprocess_mnist(batch_size):
         train_loader, test_loader = Preprocess.load_mnist(batch_size)
 
-        def scale_to_neg_one_to_one(tensor):
-            return tensor * 2 - 1
+        def scale(min_val, max_val, tensor):
+            # Scale to [0,1] first
+            tensor = (tensor - tensor.min()) / (tensor.max() - tensor.min())
+            # Then scale to [min_val, max_val]
+            tensor = tensor * (max_val - min_val) + min_val
+            return tensor
 
-        # Scale images to [-1, 1]
-        for loader in [train_loader, test_loader]:
-            for batch in loader:
-                images, labels = batch
-                images = scale_to_neg_one_to_one(images)
+        # Create new loaders with transformed data
+        transformed_train_loader = DataLoader(
+            Preprocess.TransformedDataset(train_loader.dataset, lambda x: scale(-1, 1, x)), 
+            batch_size=batch_size, 
+            shuffle=True
+        )
+        transformed_test_loader = DataLoader(
+            Preprocess.TransformedDataset(test_loader.dataset, lambda x: scale(-1, 1, x)), 
+            batch_size=batch_size, 
+            shuffle=False
+        )
 
-        return train_loader, test_loader
-    
+        return transformed_train_loader, transformed_test_loader
+        
     # print the shape of the images
     def print_shape(loader):
         for batch in loader:
             images, labels = batch
             print(images.shape)
             break
+    
+    def save_mnist_image(loader, save_dir, index=0):
+        """
+        Save a single MNIST image from the provided loader to a specified directory.
+        
+        Args:
+            loader: DataLoader containing MNIST images
+            save_dir: Directory where the image will be saved
+            index: Index of the image to save (default: 0)
+            
+        Returns:
+            tuple: (save_path, label) of the saved image
+        """
+        try:
+            # Create save directory if it doesn't exist
+            os.makedirs(save_dir, exist_ok=True)
+            
+            # Get the dataset from the loader
+            dataset = loader.dataset
+            
+            # Get the image and label
+            if isinstance(dataset, Subset):
+                image, label = dataset.dataset[dataset.indices[index]]
+            else:
+                image, label = dataset[index]
+            
+            # Convert image for saving
+            # if isinstance(image, torch.Tensor):
+            #     # If image is scaled to [-1, 1], rescale to [0, 1]
+            #     if image.min() < 0:
+            #         image = (image + 1) / 2
+                
+            #     # Convert to PIL Image
+            #     image = transforms.ToPILImage()(image)
+            
+            # Create filename with index and label
+            filename = f'mnist_image_{index}_label_{label}.png'
+            save_path = os.path.join(save_dir, filename)
+            
+            # Save the image
+            image.save(save_path)
+            print(f"Image saved successfully to {save_path}")
+            
+            return save_path, label
+            
+        except Exception as e:
+            print(f"Error saving image: {str(e)}")
+            return None, None
 
 if __name__ == '__main__':
     train_loader, test_loader = Preprocess.preprocess_mnist(64)
-    print('Train:', len(train_loader.dataset))
-    print('Test:', len(test_loader.dataset))
-    Preprocess.print_shape(train_loader)
+    # Preprocess.save_mnist_image(train_loader, save_dir='mnist_images', index=0)
+    # Preprocess.print_shape(train_loader)
+    print(train_loader.dataset[0])
+    # print('Train:', len(train_loader.dataset))
+    # print('Test:', len(test_loader.dataset))
+    # Preprocess.print_shape(train_loader)
