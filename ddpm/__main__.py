b4dcfdca58651ca01ed1839318bd36f22f480f36
JackfriskDTU <s194527@student.dtu.dk>
2024-11-15 17:01:13 +0100

    feat: update main script to use U-Net model and restructure configuration parameters

--- ddpm/__main__.py
+++ ddpm/__main__.py
@@ -6,7 +6,7 @@ import os
 import sys
 import torch
 
-from model import SimpleNN
+from model import UNet
 from utils import set_project_root
 from validate import validate
 
@@ -17,27 +17,29 @@ def main(cfg: DictConfig):
     set_project_root()
 
     # Define the network
-    input_size = cfg.input_size
-    hidden_size = cfg.hidden_size
-    output_size = cfg.output_size
-    learning_rate = cfg.learning_rate
-    batch_size = cfg.batch_size
-    epochs = cfg.epochs
+    input_size = cfg.model.input_size
+    hidden_size = cfg.model.hidden_size
+    output_size = cfg.model.output_size
+    in_channels = cfg.model.in_channels
+    out_channels = cfg.model.out_channels
+    time_dim = cfg.model.time_dim
+    learning_rate = cfg.training.learning_rate
+    batch_size = cfg.training.batch_size
+    epochs = cfg.training.epochs
 
-    model = SimpleNN(input_size, hidden_size, output_size)
 
-    # Create a dummy input tensor
-    input_tensor = torch.randn(1, input_size)
+    model = UNet(in_channels, out_channels, time_dim)
 
-    # Forward pass
-    output = model(input_tensor)
-    print(output)
+    # test the model
+    x = torch.randn(batch_size, in_channels, input_size, input_size)
+    t = torch.randint(0, 10, (1, batch_size))
+    y = model(x, t, verbose=False)
 
-    # Create dummy validation data
-    validation_data = [(torch.randn(1, input_size), torch.tensor([1])) for _ in range(5)]
+    # # Create dummy validation data
+    # validation_data = [(torch.randn(1, input_size), torch.tensor([1])) for _ in range(5)]
 
-    # Validate the model
-    validate(model, validation_data)
+    # # Validate the model
+    # validate(model, validation_data)
 
 if __name__ == "__main__":
     main()
