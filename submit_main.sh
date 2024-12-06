1fc533720202e6441940ed325f5915bb3a389d0f
JackfriskDTU <s194527@student.dtu.dk>
2024-12-06 10:52:02 +0100

    fix: update job configuration in submit_main.sh for new paths and resource allocation

--- submit_main.sh
+++ submit_main.sh
@@ -3,32 +3,32 @@
 ### –- specify queue --
 #BSUB -q gpua100
 ### -- set the job Name --
-#BSUB -J 202_DDPM_22nov
+#BSUB -J 202_DDPM_2dec
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 8
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 24:00
+#BSUB -W 02:00
 # request 5GB of system-memory
-#BSUB -R "rusage[mem=40GB]"
+#BSUB -R "rusage[mem=20GB]"
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-##BSUB -u s194545@dtu.dk
+##BSUB -u s194524@dtu.dk
 ### -- send notification at start --
 #BSUB -B
 ### -- send notification at completion--
 #BSUB -N
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -o /dtu/blackhole/02/146594/DDPM_deep_learning_202/std/gpu_%J.out
-#BSUB -e /dtu/blackhole/02/146594/DDPM_deep_learning_202/std/gpu_%J.err
+#BSUB -o /work3/s194527/DDPM_deep_learning_202/std/gpu_%J.out
+#BSUB -e /work3/s194527/DDPM_deep_learning_202/std/gpu_%J.err
 # -- end of LSF options --
 
-source /zhome/cc/c/146594/miniconda3/bin/activate
+source /work3/s194527/02456-deep-learning-with-PyTorch/miniconda3/bin/activate
 conda activate ddpm_env
 
-cd /dtu/blackhole/02/146594/DDPM_deep_learning_202
+cd /work3/s194527/DDPM_deep_learning_202
 
 python ddpm
